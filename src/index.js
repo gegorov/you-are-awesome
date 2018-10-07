5cfec120c2fbffedaccce40e9fea775220dad9bf
gegorov <gegorov@yahoo.com>
2018-10-07 21:31:14 +0300

    feat: add solution to 7 out of 10 test

--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = property => property;
+
+
+const createNotEnumerableProperty = (property) => {
+  /* eslint-disable */
+  Object.defineProperty(
+    Object.prototype,
+    property,
+    {
+      value: 'value',
+      enumerable: false,
+    },
+  );
+  /* eslint-enable */
+  return property;
+};
+
+const createProtoMagicObject = () => {
+  const magic = () => {};
+  magic.prototype = magic.__proto__; // eslint-disable-line
+  return magic;
+};
+const incrementor = () => {
+
+};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = param => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(param);
+  }, 1000);
+  setTimeout(() => {
+    reject(new Error());
+  }, 2000);
+});
+
+const getDeepPropertiesCount = (object) => {
+  let counter = 0;
+
+  const helper = (obj) => {
+    /* eslint-disable */
+    for (const subObj in obj) {
+      counter += 1;
+      helper(obj[subObj]);
+      
+    }
+      /* eslint-enable */
+  };
+
+  helper(object);
+  return counter;
+};
+const createSerializedObject = () => {
+  const object = { first: 'x', second: 'y' };
+  return new String(JSON.stringify(object)); // eslint-disable-line
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => arr.sort((a, b) => a.__proto__ - b.__proto__); // eslint-disable-line
+
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
@@ -23,4 +71,5 @@ exports.createIncrementer = createIncrementer;
 exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
+exports.toBuffer = toBuffer;
 exports.sortByProto = sortByProto;
